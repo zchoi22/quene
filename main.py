1f11a782133080f48a9771e3a8fc7fa13b82ff2d
Zion Choi <zchoi22@kentdenver.org>
2021-12-15 16:20:29 -0700

    added comments and headers

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ if __name__ == '__main__':
     test_node2 = qn(2)
     test_node3 = qn(3)
 
+    #test 1 checks enquene for first node
+    #and quene size
     print('Test 1')
     test_quene = quene()
     test_quene.enquene(test_node1)
     print('Top Node: ('+test_quene.peek().toString()+')')
     print('Current size: '+str(test_quene.size()))
 
+    #test 2 tests dequene and size again
     print('\nTest 2')
     print(test_quene.dequene().toString())
     print('Current size: '+str(test_quene.size()))
 
+    #test 3 tests quene with multiple nodes
     print('\nTest 3')
     test_quene.enquene(test_node2)
     test_quene.enquene(test_node3)
--- quene.py
+++ quene.py
@@ -1,11 +1,14 @@
-
+#main class for quene
 class quene:
 
+    #empty constructor
     def __init__(self):
         self.first = None
         self.last = None
         self.length = 0
 
+    #enquene method, if length is 0, then it
+    #runs differently
     def enquene(self, element):
         if self.length == 0:
             self.first = element
@@ -18,6 +21,10 @@ class quene:
 
         self.length+=1
 
+    #if there is more than 1 node, it will
+    #set the second node to the first and
+    #return the previous top node. If there
+    #is only one node, it will remove and return it
     def dequene(self):
         try:
             self.first.get_next().set_prev(None)
@@ -30,11 +37,15 @@ class quene:
             self.first = None
             return temp
 
+    #returns boolen, is empty
     def isEmpty(self):
         return self.length==0
 
+    #returns size (self.length)
     def size(self):
         return self.length
 
+    #returns the top node without
+    #removing it
     def peek(self):
         return self.first
