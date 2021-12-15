64ccc7d88e28045d97c24f1b75150d1cff3f6e3c
Zion Choi <zchoi22@kentdenver.org>
2021-12-15 15:35:01 -0700

    new node class for quene in Python

---
+++ quene_node.py
@@ -0,0 +1,44 @@
+#epic new quene node
+
+class quene_node:
+
+#since python doesn't support multiple constructors,
+#we will overload it, and for each item in args,
+#we will construct it differently.
+    def __init__(self, *args):
+        #empty constructor
+        if args == ():
+            self.data = None
+            self.prev = None
+            self.next = None
+
+        elif len(args) == 1:
+            self.data = args[0]
+            self.prev = None
+            self.next = None
+
+        else:
+            self.data = args[0]
+            self.prev = args[1]
+            self.next = args[2]
+
+    def get_data(self):
+        return self.data
+
+    def get_prev(self):
+        return self.prev
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, data):
+        self.data = data
+
+    def set_prev(self, prev):
+        self.prev = prev
+
+    def set_next(self, next):
+        self.next = next
+
+    def toString(self):
+        return "Node: "+self.data
